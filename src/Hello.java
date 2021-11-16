850b16c46c44ea82866c9c20cf0b6e2e72f32bd6
Gari1996 <84247119+Gari1996@users.noreply.github.com>
2021-11-15 23:33:17 -0800

    commit by team member

--- src/Hello.java
+++ src/Hello.java
@@ -6,6 +6,10 @@ public class Hello {
 
 		System.out.println("This is git demo project");
 		System.out.println("to show how git works with Eclipse");
+	    int a = 5;
+	    int b = 10;
+	    if(a<b)
+	    	System.out.println("a is less than b");
 	}
 
 }
